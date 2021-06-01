f99a26550b7fccf0ccbc81f6eb7088279f99fb7d
sleepymarcy <61689492+sleepymarcy@users.noreply.github.com>
2021-06-01 20:54:41 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+import sys
+import random
+import pygame
+
+# Martyna
+plik_obrazu = "C:/Marcy/Dev/Repo/Cwieczenie_Puzzle/blazenek_resized.jpg" 
+rozmiar_grafiki = (800, 600)
+szerokosc_kafelka = 200
+wysokosc_kafelka = 200
+kolumny = 4
+rzedy = 3
+
+# w prawym dolnym rogu nie ma kafelka
+pusty_kafelek = (kolumny-1, rzedy-1)   
+
+czarny = (0, 0, 0)
+
+# poziome i pionowe granice dla kafelków
+pozioma_granica = pygame.Surface((szerokosc_kafelka, 1))
+pozioma_granica.fill(czarny)
+pionowa_granica = pygame.Surface((1, wysokosc_kafelka))
+pionowa_granica.fill(czarny)

dc406d9b30355a3ee3d5beabe932bbd3aec012dd
Martyna <martyna.sowinska@hotmail.com>
2021-06-05 16:51:42 +0200

    Dzielenie obrazu i tworzenie granic

--- main.py
+++ main.py
@@ -21,4 +21,22 @@ pozioma_granica.fill(czarny)
 pionowa_granica = pygame.Surface((1, wysokosc_kafelka))
 pionowa_granica.fill(czarny)
 
-# nowy testing 4, 5, 6
+obraz = pygame.image.load(plik_obrazu)
+kafelki = {}
+for k in range(kolumny) :
+    for r in range(rzedy) :
+        kafelek = obraz.subsurface (
+            k*szerokosc_kafelka, r*wysokosc_kafelka, 
+            szerokosc_kafelka, wysokosc_kafelka)
+        kafelki [(k, r)] = kafelek
+        if (k, r) != pusty_kafelek:
+            kafelek.blit(pozioma_granica, (0, 0))
+            kafelek.blit(pozioma_granica, (0, wysokosc_kafelka-1))
+            kafelek.blit(pionowa_granica, (0, 0))
+            kafelek.blit(pionowa_granica, (szerokosc_kafelka-1, 0))
+            # tutaj zaokrąglamy krawędzie 
+            kafelek.set_at((1, 1), czarny)
+            kafelek.set_at((1, wysokosc_kafelka-2), czarny)
+            kafelek.set_at((szerokosc_kafelka-2, 1), czarny)
+            kafelek.set_at((szerokosc_kafelka-2, wysokosc_kafelka-2), czarny)
+kafelki[pusty_kafelek].fill(czarny)

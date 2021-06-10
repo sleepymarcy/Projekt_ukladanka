1c59d1e5a0ac561af9cc875fc7841bb991e22870
Martyna <martyna.sowinska@hotmail.com>
2021-06-10 12:47:00 +0200

    Usunięcie komentarzy

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import sys
 import random
 import pygame
 
-# Martyna
 plik_obrazu = "C:/Marcy/Dev/Repo/projekcik/Projekt_ukladanka/blazenek_resized.jpg"
 rozmiar_grafiki = (800, 600)
 szerokosc_kafelka = 200
@@ -10,12 +9,10 @@ wysokosc_kafelka = 200
 kolumny = 4
 rzedy = 3
 
-# w prawym dolnym rogu nie ma kafelka
 pusty_kafelek = (kolumny-1, rzedy-1)
 
 czarny = (0, 0, 0)
 
-# poziome i pionowe granice dla kafelków
 pozioma_granica = pygame.Surface((szerokosc_kafelka, 1))
 pozioma_granica.fill(czarny)
 pionowa_granica = pygame.Surface((1, wysokosc_kafelka))
@@ -34,7 +31,6 @@ for k in range(kolumny) :
             kafelek.blit(pozioma_granica, (0, wysokosc_kafelka-1))
             kafelek.blit(pionowa_granica, (0, 0))
             kafelek.blit(pionowa_granica, (szerokosc_kafelka-1, 0))
-            # tutaj zaokrąglamy krawędzie
             kafelek.set_at((1, 1), czarny)
             kafelek.set_at((1, wysokosc_kafelka-2), czarny)
             kafelek.set_at((szerokosc_kafelka-2, 1), czarny)
